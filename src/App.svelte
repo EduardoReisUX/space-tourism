d678ae9700e42258e95dcc6b190802aed68a2546
EduardoReisUX <ducabruno09@hotmail.com>
2022-04-08 09:40:49 -0400

    Inital commit: tailwind, theme, assests, data...

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>SSS | Space Tourism Svelte</title>
+</svelte:head>
+
+<main class="min-h-screen grid place-items-center bg-brand-black">
+  <h1 class="text-h1 text-brand-white font-serif">SPACE</h1>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    colors: {
+      brand: {
+        primary: "#D0D6F9",
+        black: "#0B0D17",
+        white: "#FFFFFF",
+      },
+    },
+    fontFamily: {
+      serif: ["Bellefair", "serif"],
+      sans: ["Barlow", "sans-serif"],
+      mono: ["Barlow Condensed", "sans-serif"],
+    },
+    fontSize: {
+      nav: [
+        "16px",
+        {
+          letterSpacing: "2.7",
+        },
+      ],
+      body: "18px",
+      subh2: [
+        "14px",
+        {
+          letterSpacing: "2.35px",
+        },
+      ],
+      subh1: "28px",
+      h5: [
+        "28px",
+        {
+          letterSpacing: "4.75px",
+        },
+      ],
+      h4: "32px",
+      h3: "56px",
+      h2: "100px",
+      h1: "150px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
