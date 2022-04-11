fb2b1836ba61af3f1e66cffc8ebfb18213fb1fb8
EduardoReisUX <ducabruno09@hotmail.com>
2022-04-11 17:00:52 -0400

    feat: add bg images for all devices

--- src/components/shared/Background.svelte
+++ src/components/shared/Background.svelte
@@ -1,25 +1,76 @@
 <script lang="ts">
-  import homeBg from "./../../assets/home/background-home-mobile.jpg";
-  import destinationBg from "./../../assets/destination/background-destination-mobile.jpg";
-  import crewBg from "./../../assets/crew/background-crew-mobile.jpg";
-  import techBg from "./../../assets/technology/background-technology-mobile.jpg";
+  /* Mobile bg images */
+  import homeMobile from "./../../assets/home/background-home-mobile.jpg";
+  import destinationMobile from "./../../assets/destination/background-destination-mobile.jpg";
+  import crewMobile from "./../../assets/crew/background-crew-mobile.jpg";
+  import techMobile from "./../../assets/technology/background-technology-mobile.jpg";
 
-  type BackgroundType = keyof typeof backgrounds;
+  /* Tablet bg images */
+  import homeTablet from "./../../assets/home/background-home-tablet.jpg";
+  import destinationTablet from "./../../assets/destination/background-destination-tablet.jpg";
+  import crewTablet from "./../../assets/crew/background-crew-tablet.jpg";
+  import techTablet from "./../../assets/technology/background-technology-tablet.jpg";
 
-  let backgrounds = {
-    home: homeBg,
-    destination: destinationBg,
-    crew: crewBg,
-    techBg: techBg,
+  /* Desktop bg images */
+  import homeDesktop from "./../../assets/home/background-home-desktop.jpg";
+  import destinationDesktop from "./../../assets/destination/background-destination-desktop.jpg";
+  import crewDesktop from "./../../assets/crew/background-crew-desktop.jpg";
+  import techDesktop from "./../../assets/technology/background-technology-desktop.jpg";
+
+  type BackgroundType = keyof typeof mobileBackgrounds;
+
+  let mobileBackgrounds = {
+    home: homeMobile,
+    destination: destinationMobile,
+    crew: crewMobile,
+    techBg: techMobile,
   };
 
   export let section: BackgroundType;
+
+  let tabletBackgrounds = {
+    home: homeTablet,
+    destination: destinationTablet,
+    crew: crewTablet,
+    techBg: techTablet,
+  };
+
+  let desktopBackgrounds = {
+    home: homeDesktop,
+    destination: destinationDesktop,
+    crew: crewDesktop,
+    techBg: techDesktop,
+  };
+
+  let innerWidth = 0;
+  $: device =
+    innerWidth > 1024 ? "desktop" : innerWidth < 768 ? "mobile" : "tablet";
 </script>
 
+<svelte:window bind:innerWidth />
+
 {#if section}
-  <img
-    src={backgrounds[section]}
-    alt="bg"
-    class="absolute inset-0 w-full h-full object-cover -z-10"
-  />
+  {#if device === "mobile"}
+    <img
+      src={mobileBackgrounds[section]}
+      alt="bg"
+      class="absolute inset-0 w-full h-full object-cover -z-10"
+    />
+  {/if}
+
+  {#if device === "tablet"}
+    <img
+      src={tabletBackgrounds[section]}
+      alt="bg"
+      class="absolute inset-0 w-full h-full object-cover -z-10"
+    />
+  {/if}
+
+  {#if device === "desktop"}
+    <img
+      src={desktopBackgrounds[section]}
+      alt="bg"
+      class="absolute inset-0 w-full h-full object-cover -z-10"
+    />
+  {/if}
 {/if}
